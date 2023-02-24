b9b7b69955dee36a0fb5f64674008cdb445cf4af
VectorGits <davidoluremi0@gmail.com>
2023-02-24 15:16:03 +0100

    App deletes and adds todo items

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter= document.getElementById('filter');
 
 //Form submit event
 form.addEventListener('submit', addItem);
 //Delete event
 itemList.addEventListener('click', removeItem);
+//Filter event
+filter.addEventListener('keyup', filterItems);
 
 //Add item
 function addItem(e){
@@ -38,9 +41,27 @@ function addItem(e){
 //Remove item
 function removeItem(e){
     if (e.target.classList.contains('delete')) {
-        if (confirm('Are you sure?')) {
+        if (confirm('Are you done with this task?')) {
             var li = e.target.parentElement;
             itemList.removeChild(li);
         }
     }
+}
+
+//Filter Items
+function filterItems(e) {
+    //convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    //Get list
+    var items = itemList.getElementsByTagName('li');
+    //convert to an array
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        console.log(itemName);
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+});
 }
